8ffb195a06a63056ba1eb26ce9ebb7748ad0e6ea
uday <udaysubba2004@gmail.com>
2025-02-16 20:35:07 +0530

    init fastapi backend for smart service

---
+++ README.md
@@ -0,0 +1,2 @@
+#Smart Service
+A web app to connect local service providers with their clients
---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI(
+    root_path="/api",
+)
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", name= "Api Status")
+async def root():
+    return {"status":"true", "message": "Smart Services Api server is running"}
