8bc10943822bdd16d338e2ed2a91e39d6506cb20
uday <udaysubba2004@gmail.com>
2025-02-18 19:58:49 +0530

    update user model

--- app/database/models/userModel.py
+++ app/database/models/userModel.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    email: str
+    email: str = Field(unique=True)
+    phone: str = Field(unique=True)
     name: str
     password: str
 
 class UserCreate(BaseModel):
     email: str
+    phone: str
     name: str
     password: str
